58f749aa69ec7a9b4f2acc652c3e5f1cdec3ac2c
Juan Cardona <juanscr24@hotmail.com>
2026-07-17 17:02:13 -0500

    Se añade toda la historia de usuario 2 por completo

--- SQL H2/DDL.sql
+++ SQL H2/DDL.sql
@@ -1,19 +1,20 @@
-CREATE DATABASE gestion_academica_universidad
+CREATE DATABASE gestion_academica_universidad;
+USE gestion_academica_universidad;
 
 CREATE TABLE estudiantes (
-    id_estudiante SERIAL PRIMARY KEY,
+    id_estudiante INT AUTO_INCREMENT PRIMARY KEY,
     fecha_nacimiento DATE NOT NULL,
     ciudad VARCHAR(100) NOT NULL,
     nombre_completo VARCHAR(100) NOT NULL,
     correo_electronico VARCHAR(100) NOT NULL,
-    genero CHAR(1) CHECK (genero IN ('M', 'F')) NOT NULL,
+    genero CHAR(1) NOT NULL CHECK (genero IN ('M', 'F')) ,
     identificacion VARCHAR(100) UNIQUE NOT NULL,
     carrera VARCHAR(100) NOT NULL,
     fecha_ingreso DATE NOT NULL
 );
 
 CREATE TABLE docentes (
-    id_docente SERIAL PRIMARY KEY,
+    id_docente INT AUTO_INCREMENT PRIMARY KEY,
     nombre_completo VARCHAR (50) NOT NULL,
     correo_electronico VARCHAR (100) NOT NULL,
     departamento_academico VARCHAR(50) NOT NULL ,
@@ -21,24 +22,23 @@ CREATE TABLE docentes (
 );
 
 CREATE TABLE cursos (
-    id_curso SERIAL PRIMARY KEY,
+    id_curso INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR (100) NOT NULL,
     codigo INT UNIQUE NOT NULL,
     creditos INT NOT NULL,
-    semestre INT CHECK (semestre BETWEEN 1 AND 10) NOT NULL,
+    semestre INT NOT NULL CHECK (semestre BETWEEN 1 AND 10),
     id_docente INT, 
 
     CONSTRAINT fk_docente_id FOREIGN KEY (id_docente) REFERENCES docentes(id_docente)
 );
 
 CREATE TABLE inscripciones (
-    id_inscripcion SERIAL PRIMARY KEY,
+    id_inscripcion INT AUTO_INCREMENT PRIMARY KEY,
     id_estudiante INT,
     id_curso INT,
     fecha_inscripcion DATE, 
     calificacion_final NUMERIC(3,2) CHECK (calificacion_final BETWEEN 0.0 AND 5.0), 
 
-    CONSTRAINT fk_estudiante FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id_estudiante)
+    CONSTRAINT fk_estudiante FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id_estudiante),
     CONSTRAINT fk_curso FOREIGN KEY (id_curso) REFERENCES cursos(id_curso)
 );
-
